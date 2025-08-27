c9288f4518284fec76ee99b94d6a855ca5d3e295
Iskander <iskandar.13.aburahmonov@gmail.com>
2025-08-27 17:07:31 +0500

    first commit

---
+++ app/(root)/(tabs)/explore.tsx
@@ -0,0 +1,12 @@
+import {Text, View} from "react-native";
+import {SafeAreaView} from "react-native-safe-area-context";
+
+const Explore = () => {
+    return (
+        <SafeAreaView>
+            <Text>Explore</Text>
+        </SafeAreaView>
+    )
+}
+
+export default Explore
